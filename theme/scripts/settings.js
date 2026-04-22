4994503f2c4a36a2dadd1a3763ad9eda4f609118
Yugoslavskiy Daniil <yugoslavskiy@gmail.com>
2026-04-22 09:55:53 +0300

    Update documentation

---
+++ theme/scripts/settings.js
@@ -0,0 +1,3 @@
+var base_url = "/";
+var build_uuid = "c3a6e041-5599-6cbb-ddc3-f2152d1abe8e";
+var tour_steps = {'matrix': 'matrices/russia', 'technique': 'techniques/T0048', 'subtechnique': 'techniques/T0048/009', 'relationships': {'step1': 'T0049', 'step3': ['T0048-002', 1], 'obj_id': 'groups/G0008'}};
